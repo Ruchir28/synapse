5f0e084c1f719f1339180c2fde0b2bd985b80605
Ruchir28 <ruchirnakra28@gmail.com>
2025-09-06 18:41:19 +0530

    feat: Add mean and mean_axis methods to ReductionOps trait for NDArray

--- src/ops/reduction.rs
+++ src/ops/reduction.rs
@@ -1,28 +1,38 @@
 use crate::NDArray;
-use std::ops::Add;
+use std::{ops::Add, vec};
+use num_traits::{Zero, ToPrimitive};
+
 
 pub trait ReductionOps<T> {
+
+    type MeanOutput;
+
     fn sum(&self) -> T;
     fn sum_axis(&self, axis: usize) -> NDArray<T>;
+    fn mean(&self) -> Self::MeanOutput;
+    fn mean_axis(&self, axis: usize) -> NDArray<Self::MeanOutput>;
 }
 
 impl<T> ReductionOps<T> for NDArray<T>
 where
-    T: Copy + Clone + Add<Output = T> + Default,
+    T: Copy + Clone + Add<Output = T> + Default + ToPrimitive + Zero,
 {
+
+    type MeanOutput = f64;
+
     fn sum(&self) -> T {
         self.iter().fold(T::default(), |acc, &x| acc + x)
     }
 
     fn sum_axis(&self, axis: usize) -> NDArray<T> {
-        let mut output_dims = self.dims().to_vec();
+        let mut output_dims: Vec<usize> = self.dims().to_vec();
         if axis >= output_dims.len() {
             panic!("Axis out of bounds");
         }
         output_dims.remove(axis);
 
         let mut result = NDArray::new(
-            vec![T::default(); output_dims.iter().product()],
+            vec![T::zero(); output_dims.iter().product()],
             output_dims,
         );
 
@@ -37,6 +47,29 @@ where
 
         result
     }
+
+    fn mean(&self) -> Self::MeanOutput {
+        let sum: f64 = self.iter().map(|&x| x.to_f64().expect("mean undefined for this dtype")).sum();
+        let product = self.dims().iter().product::<usize>() as f64;
+        return sum / product;
+    }
+
+    fn mean_axis(&self, axis: usize) -> NDArray<Self::MeanOutput> {
+
+        let axis_sum = self.sum_axis(axis);
+
+        let mean_axis_len = self.dims()[axis] as f64;
+
+        let mean_data = axis_sum.iter().map(|x| x.to_f64()
+        .expect("mean undefined for this dtype") / mean_axis_len).collect();
+
+        let mut updated_dims = self.dims().to_vec();
+
+        updated_dims.remove(axis);
+
+        return NDArray::new(mean_data,updated_dims);
+    }
+
 }
 
 #[cfg(test)]
@@ -72,4 +105,56 @@ mod tests {
         let array = NDArray::new(vec![1, 2, 3, 4, 5, 6], vec![2, 3]);
         array.sum_axis(2);
     }
+
+    #[test]
+    fn test_mean() {
+        let array = NDArray::new(vec![1, 2, 3, 4, 5, 6], vec![2, 3]);
+        assert_eq!(array.mean(),21.0/6.0);
+    }
+
+    #[test]
+    fn test_mean_axis_0() {
+        let array = NDArray::new(vec![1, 2, 3, 4, 5, 6], vec![2, 3]);
+        let result = array.mean_axis(0);
+        assert_eq!(result.dims(), &[3]);
+        assert_eq!(result.data().as_ref(), &vec![2.5, 3.5, 4.5]);
+    }
+
+    #[test]
+    fn test_mean_axis_1() {
+        let array = NDArray::new(vec![1, 2, 3, 4, 5, 6], vec![2, 3]);
+        let result = array.mean_axis(1);
+        assert_eq!(result.dims(), &[2]);
+        assert_eq!(result.data().as_ref(), &vec![2.0, 5.0]);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_mean_axis_out_of_bounds() {
+        let array = NDArray::new(vec![1, 2, 3, 4, 5, 6], vec![2, 3]);
+        array.mean_axis(2);
+    }
+
+    #[test]
+    fn test_mean_u8() {
+        let array: NDArray<u8> = NDArray::new(vec![1_u8, 2_u8, 3_u8, 4_u8, 5_u8, 6_u8], vec![2, 3]);
+        assert_eq!(array.mean(), 21.0/6.0);
+    }
+
+    #[test]
+    fn test_mean_axis_0_u8() {
+        let array: NDArray<u8> = NDArray::new(vec![1_u8, 2_u8, 3_u8, 4_u8, 5_u8, 6_u8], vec![2, 3]);
+        let result = array.mean_axis(0);
+        assert_eq!(result.dims(), &[3]);
+        assert_eq!(result.data().as_ref(), &vec![2.5, 3.5, 4.5]);
+    }
+
+    #[test]
+    fn test_mean_axis_1_u8() {
+        let array: NDArray<u8> = NDArray::new(vec![1_u8, 2_u8, 3_u8, 4_u8, 5_u8, 6_u8], vec![2, 3]);
+        let result = array.mean_axis(1);
+        assert_eq!(result.dims(), &[2]);
+        assert_eq!(result.data().as_ref(), &vec![2.0, 5.0]);
+    }
+    
 }
